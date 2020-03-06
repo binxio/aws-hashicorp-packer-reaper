fb2b5095442a5f1c7aedbb8977d08d5a73c6179f
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2020-03-06 09:36:21 +0100

    bumped to version 0.1.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='aws-hashicorp-packer-reaper',
-    version="0.1.1",
+    version="0.1.2",
     url='https://github.com/binxio/aws-hashicorp-packer-reaper',
     license='BSD',
     author='Mark van Holsteijn',
