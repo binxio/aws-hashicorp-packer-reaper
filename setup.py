a0fe34e5ed4f26d49d107e295461b73aa1c5cc48
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2023-09-20 08:59:01 +0200

    chore: upgraded to python 3.9

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='aws-hashicorp-packer-reaper',
-    version="0.2.0",
+    version="0.3.0",
     url='https://github.com/binxio/aws-hashicorp-packer-reaper',
     license='BSD',
     author='Mark van Holsteijn',
